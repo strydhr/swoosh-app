97c2ee83f72eee72828ce3ef715ecb5e701a4722
Satyia Anand <satyia@Satyias-iMac.local>
2018-01-22 15:58:33 +0800

    interface action Button (IBAction)

--- swoosh-app/swoosh-app/Controller/TypeVC.swift
+++ swoosh-app/swoosh-app/Controller/TypeVC.swift
@@ -9,18 +9,40 @@
 import UIKit
 
 class TypeVC: UIViewController {
+    
+    var player:Player!
 
    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player=Player()
     }
 
     @IBAction func nextBtnPressed(_ sender: Any) {
      performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
 
+    @IBAction func onMenTapped(_ sender: Any) {
+        selectedType(Type: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        //player.selectedType = "womens"
+        //nextBtn.isEnabled = true        will be changed to (using the func below)
+        selectedType(Type: "womens")
+    }
+    
+    @IBAction func onMixedTapped(_ sender: Any) {
+       selectedType(Type: "mixed")
+    }
+    
+    func selectedType(Type: String) {
+        player.selectedType = Type
+        nextBtn.isEnabled = true
+    }
     /*
     // MARK: - Navigation
 
---
+++ swoosh-app/swoosh-app/Model/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  swoosh-app
+//
+//  Created by Satyia Anand on 22/01/2018.
+//  Copyright © 2018 Satyia Anand. All rights reserved.
+//
+
+import Foundation
+struct Player {
+    var selectedType:String?
+    var selectedSkills:String?
+}
